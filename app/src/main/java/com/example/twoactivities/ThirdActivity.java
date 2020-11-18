a553e82765e2893d9664f7c041872560c5ce2398
Sergiu Alexandrescu <alexandrescu.sergiu@gmail.com>
2020-11-18 14:51:27 +0200

    Added Button logic

--- app/src/main/java/com/example/twoactivities/ThirdActivity.java
+++ app/src/main/java/com/example/twoactivities/ThirdActivity.java
@@ -3,6 +3,8 @@ package com.example.twoactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ThirdActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class ThirdActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
+    }
+
+    public void onClick(View view) {
+        Toast.makeText(this,"Button was clicked",Toast.LENGTH_LONG).show();
     }
 }
